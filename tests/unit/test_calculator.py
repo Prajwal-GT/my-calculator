1291678c144b358551afdcb66016692a673458a3
Prajwal-GT <zorrovijay2@gmail.com>
2026-09-02 16:01:07 +0530

    Format tests with Black

--- tests/unit/test_calculator.py
+++ tests/unit/test_calculator.py
@@ -111,6 +111,7 @@ class TestAdvancedOperations:
             ValueError, match="Cannot calculate square root of negative number"
         ):
             square_root(-4)
+
     def test_power_negative_exponent(self):
         """Test power with a negative exponent"""
         assert power(2, -2) == 0.25
